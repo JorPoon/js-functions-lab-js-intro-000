62045d1e95b067d992d0ce8e069390ed6648bd30
Jor Ming Poon <jormpoon@gmail.com>
2018-08-06 22:06:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return 'Happy ' + `${holiday}, ` + `${name}!`
 }
 
-function holdidayCountdown(holiday, days) {
+function holidayCountdown(holiday, days) {
   return 'It\'s ' + `${days}` + ' days until ' + `${holiday}!`
 }
